8f1b6bc621fba74b38a5f643de6449acad8b8538
Vicente Canales <vicente.canales@automattic.com>
2022-07-30 15:30:31 -0400

    remove static serve from api

--- api/index.js
+++ api/index.js
@@ -1,13 +1,8 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import Fastify from 'fastify';
-import fastifyStatic from '@fastify/static';
 
 const fastify = Fastify({ logger: true });
-
-fastify.register(fastifyStatic, {
-  root: path.resolve('web', 'public'),
-});
 
 async function list() {
   const dirs = await fs.readdir(path.resolve('gitignore'));
@@ -18,10 +13,6 @@ fastify.get('/api', async () => {
   const data = await list();
 
   return { data };
-});
-
-fastify.get('/', async (request, reply) => {
-  return reply.sendFile('index.html');
 });
 
 const start = async () => {
