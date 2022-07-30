8e28b78700feea07014fbe858c45c0bae88e3f5c
Vicente Canales <vicente.canales@automattic.com>
2022-07-30 03:16:10 -0400

    serve static files

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 import { promises as fs } from 'fs';
+import path from 'path';
 import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
 
 const fastify = Fastify({ logger: true });
 
+fastify.register(fastifyStatic, {
+  root: path.resolve('public'),
+  prefix: '/public/',
+});
+
 async function list() {
-	const dirs = await fs.readdir('./gitignore');
-	
-	return dirs;
+  const dirs = await fs.readdir(path.resolve('gitignore'));
+  return dirs;
 }
 
+fastify.get('/api', async () => {
+  const data = await list();
+
+  return { data };
+});
+
 fastify.get('/', async (request, reply) => {
-	const data = await list();
-	return { data };
+  return reply.sendFile('index.html');
 });
 
 const start = async () => {
-	try {
-		await fastify.listen({ port: 3000 });
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
-}
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
 
 start();
